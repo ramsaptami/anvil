d7a0754c4ec83103475995ed2cab73639ae803d6
saptami <saptami@appsmith.com>
2024-05-31 10:06:10 +0000

    System generated commit, initial commit

---
+++ pages/single widget in a zone/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,33 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		let a
+		return a = [
+  {
+    "id": "userId",
+    "label": "userId",
+    "isVisible": true
+  },
+  {
+    "id": "id",
+    "label": "id",
+    "isVisible": true
+  },
+  {
+    "id": "title",
+    "label": "title",
+    "isVisible": true
+  },
+  {
+    "id": "body",
+    "label": "body",
+    "isVisible": true
+  }
+		]
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
